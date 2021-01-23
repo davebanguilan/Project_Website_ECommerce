5ee9b70a986bff9ca383c67a24ebc0ace1cef437
davebanguilan <desbanguilan@gmail.com>
2021-01-23 23:42:30 +0800

    Initial Commit, Added Products and Navbar

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+
+import Product from "./Product/Product";
+import useStyles from "./styles";
+
+const products = [
+  {
+    id: 1,
+    name: "Shoes",
+    description: "Running Shoes.",
+    price: "$10",
+    image:
+      "https://images.unsplash.com/photo-1465453869711-7e174808ace9?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=755&q=80",
+  },
+  {
+    id: 2,
+    name: "Macbook",
+    description: "Apple Macbook.",
+    price: "$10",
+    image:
+      "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80",
+  },
+];
+
+const Products = () => {
+  const classes = useStyles();
+  return (
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
+      <Grid container justify="center" spacing={4}>
+        {products.map((product) => (
+          <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+            <Product product={product} />
+          </Grid>
+        ))}
+      </Grid>
+    </main>
+  );
+};
+
+export default Products;
